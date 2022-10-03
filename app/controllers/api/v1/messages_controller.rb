3deea9f451d93322ad749d929807ac0473bc34da
Arturgouveia1970 <artgouveiag@gmail.com>
2022-10-03 19:34:13 +0100

    linters

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
-    def index
-      @message = Message.all.sample
-      render json: @message
-    end
+  def index
+    @message = Message.all.sample
+    render json: @message
   end
-  
+end
